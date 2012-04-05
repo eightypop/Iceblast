5db5935fbbc27a0c0cfbb40b5f1a77911995cde5
Alan Liddell <alan.c.liddell@gmail.com>
2012-04-05 00:22:17 -0400

    Fixing rookie mistakes &c.
    Updated manifest again, fixed some broken in the main activity, edited
    main.xml

--- IceblastApp/gen/com/iceblaster/R.java
+++ IceblastApp/gen/com/iceblaster/R.java
@@ -14,6 +14,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
         public static final int icon=0x7f020001;
     }
+    public static final class id {
+        public static final int mainScreenListView=0x7f060000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
