ba99c7377065c451b5d073392155c4308053c8b9
Matt Thompson <whatknight@gmail.com>
2012-04-05 21:45:35 -0400

    added a station object to hold onto station info. added a list view and a list fragment to the activity. these are untested. they also will not work on android less than 3.0 api lvl 11.

--- IceblastApp/src/com/iceblaster/IceblastAppActivity.java
+++ IceblastApp/src/com/iceblaster/IceblastAppActivity.java
@@ -1,14 +1,56 @@
 package com.iceblaster;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.app.ListFragment;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.iceblaster.objects.Station;
 
 public class IceblastAppActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Create the list fragment and add it as our sole content.
+		if (getFragmentManager().findFragmentById(android.R.id.content) == null) {
+			ArrayListFragment list = new ArrayListFragment();
+			getFragmentManager().beginTransaction()
+					.add(android.R.id.content, list).commit();
+		}
+
+		final Button button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				stations.add(new Station());
+			}
+		});
+
+	}
+
+	private static ArrayList<Station> stations = new ArrayList<Station>();
+
+	public static class ArrayListFragment extends ListFragment {
+
+		@Override
+		public void onActivityCreated(Bundle savedInstanceState) {
+			super.onActivityCreated(savedInstanceState);
+			setListAdapter(new ArrayAdapter<Station>(getActivity(),
+					android.R.layout.simple_list_item_1,
+					(Station[]) stations.toArray()));
+
+		}
+
+		@Override
+		public void onListItemClick(ListView l, View v, int position, long id) {
+			Log.i("FragmentList", "Item clicked: " + id);
+		}
+	}
 }
---
+++ IceblastApp/src/com/iceblaster/objects/Station.java
@@ -0,0 +1,27 @@
+package com.iceblaster.objects;
+
+public class Station {
+
+	private String name;
+	
+	private String stationUrl;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStationUrl() {
+		return stationUrl;
+	}
+
+	public void setStationUrl(String stationUrl) {
+		this.stationUrl = stationUrl;
+	}
+	
+	
+	
+}
